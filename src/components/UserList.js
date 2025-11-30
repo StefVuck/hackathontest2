05fd6172521ea1dc4faa8338f4c2a53ea4e3e2e4
Stefan Vuckovic <stefanvuck@gmail.com>
2025-11-30 09:24:11 +0000

    Frontend Developer: Create basic frontend user interface

---
+++ src/components/UserList.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch('/api/users');
+      const data = await response.json();
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
